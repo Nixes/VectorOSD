3ca23d0f38b75f18e0de9f2efa5914dcd2059b80
Nixes <randomdude7@gmail.com>
2016-09-04 16:08:53 +1000

    finsih log object

--- example/VectorOSD_widgets.hpp
+++ example/VectorOSD_widgets.hpp
@@ -7,15 +7,31 @@ class logBox {
 
 private:
   const unsigned int padding = 10;
+  const unsigned int height = 400;
+  const unsigned int width = 400;
+  const unsigned int line_height = 20;
   unsigned int x;
   unsigned int y;
 
-  std::vector<string>;
+  std::vector<string> events;
 
+  void renderLogText(NVGcontext* vg,unsigned int tmp_x,unsigned int tmp_y,const char* text) {
+    nvgFontSize(vg, 20.0f);
+    nvgFontFace(vg, "sans");
+    nvgFillColor(vg, nvgRGBA(255,255,255,255));
+    nvgTextAlign(vg,NVG_ALIGN_LEFT|NVG_ALIGN_MIDDLE);
+    nvgText(vg, tmp_x, tmp_y, text, NULL);
+  }
+
+  void renderLogEvents(NVGcontext* vg) {
+    int events_size = events.size();
+    for(int i = 0 ; i < events_size; i++) {
+      unsigned int new_y = i * line_height;
+      renderLogText(vg,x + padding, new_y, events[i].c_str() );
+    }
+  }
 
   void renderLogBox(NVGcontext* vg) {
-  	const unsigned int height = 400;
-  	const unsigned int width = 400;
   	nvgBeginPath(vg);
 
   	// left line
@@ -40,8 +56,13 @@ public:
 
   };
 
+  void log(const char* text) {
+    string tmp_string = text;
+    events.push_back(tmp_string);
+  }
+
   void render(NVGcontext* vg) {
     renderLogBox(vg);
-
+    renderLogEvents(vg);
   }
 };
