e1e72e31e3d4a11b04c1cb6083d44984f7d6932f
Nixes <randomdude7@gmail.com>
2016-09-17 19:19:55 +1000

    add telemetry modules from MultiOSD

---
+++ telemetry/uavtalk/lib/common.h
@@ -0,0 +1,122 @@
+/*
+ * This file is part of MultiOSD <https://github.com/UncleRus/MultiOSD>
+ *
+ * MultiOSD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef TELEMETRY_UAVTALK_LIB_COMMON_H_
+#define TELEMETRY_UAVTALK_LIB_COMMON_H_
+
+#include <stddef.h>
+#include "../../../lib/pgmspace.h"
+#include "../../../lib/uart/uart.h"
+
+#define UAVTALK_RELEASE_OP150202    0
+#define UAVTALK_RELEASE_OP150502    1
+#define UAVTALK_RELEASE_LP150900    2
+#define UAVTALK_RELEASE_TL20151123  3
+#define UAVTALK_RELEASE_LP1609RC1   4
+#define UAVTALK_RELEASE_DR201604092 5
+
+#include "../../../config.h"
+
+#define UAVTALK_VERSION 0x20
+#define UAVTALK_SYNC 0x3c
+
+#define _UT_TYPE_OBJ		(UAVTALK_VERSION | 0x00)
+#define _UT_TYPE_OBJ_REQ	(UAVTALK_VERSION | 0x01)
+#define _UT_TYPE_OBJ_ACK	(UAVTALK_VERSION | 0x02)
+#define _UT_TYPE_ACK		(UAVTALK_VERSION | 0x03)
+#define _UT_TYPE_NACK		(UAVTALK_VERSION | 0x04)
+
+#define _UT_TIMEOUT_OBJID 0
+
+#define UT_NAMESPACE_OPEN namespace telemetry { namespace modules { namespace uavtalk {
+#define UT_NAMESPACE_CLOSE }}}
+
+UT_NAMESPACE_OPEN
+
+struct header_t
+{
+	uint8_t sync;
+	uint8_t msg_type;
+	uint16_t length;
+	uint32_t objid;
+	uint16_t instid;
+
+	header_t () :
+		sync (UAVTALK_SYNC), msg_type (0), length (0), objid (0), instid (0)
+	{}
+
+	header_t (uint8_t msg_type_, uint16_t length_, uint32_t objid_) :
+		sync (UAVTALK_SYNC), msg_type (msg_type_), length (length_), objid (objid_), instid (0)
+	{}
+};
+
+struct message_t
+{
+	header_t head;
+	uint8_t data [255];
+	uint8_t crc;
+};
+
+struct obj_handler_t
+{
+	typedef void (* callable_t) ();
+
+	uint32_t objid;
+	callable_t handler;
+};
+
+struct release_t
+{
+	bool instid_required;
+	uint32_t flightstatus_objid;
+	const obj_handler_t *handlers;
+	const char * const *fm_names;
+	uint8_t fm_count;
+};
+
+extern bool internal_home_calc;
+#if !defined (TELEMETRY_MODULES_I2C_BARO)
+extern bool baro_enabled;
+#endif
+#if !defined (TELEMETRY_MODULES_I2C_COMPASS)
+extern bool mag_enabled;
+#endif
+extern uint8_t release_idx;
+extern release_t release;
+extern uint8_t header_len;
+extern uint32_t telemetry_request_timeout;
+extern uint32_t connection_timeout;
+
+extern message_t buffer;
+bool parse (uint8_t b);
+
+uint8_t __attribute__ ((noinline)) get_crc (uint8_t b);
+void send (const header_t &head, uint8_t *data = NULL, uint8_t size = 0);
+void request_object (uint32_t obj_id);
+
+// flight mode name
+const char *get_fm_name_p (uint8_t fm);
+// handle current UAVObject
+bool handle ();
+// timeout, resend GCSTelemetryStats
+void update_connection ();
+// init
+void set_release ();
+
+
+UT_NAMESPACE_CLOSE
+
+#endif /* TELEMETRY_UAVTALK_LIB_COMMON_H_ */
---
+++ telemetry/uavtalk/lib/op150202/release.cpp
@@ -0,0 +1,198 @@
+/*
+ * This file is part of MultiOSD <https://github.com/UncleRus/MultiOSD>
+ *
+ * MultiOSD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "release.h"
+
+#include "stddef.h"
+#include <string.h>
+#include <math.h>
+#include "../../../telemetry.h"
+#include "../common.h"
+
+UT_NAMESPACE_OPEN
+
+namespace op150202
+{
+
+void handle_flightstatus ()
+{
+	FlightStatus *obj = (FlightStatus *) &buffer.data;
+	bool was_armed = status::armed;
+	status::armed = obj->Armed == FLIGHTSTATUS_ARMED_ARMED;
+	status::flight_mode = obj->FlightMode;
+	status::flight_mode_name_p = uavtalk::get_fm_name_p (status::flight_mode);
+	if (internal_home_calc && !was_armed && status::armed)
+		home::fix ();
+}
+
+void handle_attitudestate ()
+{
+	AttitudeState *obj = (AttitudeState *) &buffer.data;
+	attitude::roll  = obj->Roll;
+	attitude::pitch = obj->Pitch;
+	attitude::yaw   = obj->Yaw;
+}
+
+#if !defined (TELEMETRY_MODULES_I2C_BARO)
+void handle_barosensor ()
+{
+	BaroSensor *obj = (BaroSensor *) &buffer.data;
+	barometer::altitude = obj->Altitude;
+	barometer::pressure = obj->Pressure;
+	environment::temperature = barometer::temperature = obj->Temperature;
+	stable::update_alt_climb (obj->Altitude);
+	baro_enabled = true;
+}
+#endif
+
+#if !defined (TELEMETRY_MODULES_ADC_BATTERY)
+void handle_flightbatterystate ()
+{
+	FlightBatteryState *obj = (FlightBatteryState *) &buffer.data;
+	battery::battery1.cells = obj->NbCells;
+	battery::battery1.set_voltage (obj->Voltage, false);
+	battery::battery1.amperage = obj->Current;
+	battery::battery1.consumed = obj->ConsumedEnergy;
+}
+#endif
+
+void send_gcs_telemetry_stats (GCSTelemetryStatsStatus status)
+{
+	header_t h (_UT_TYPE_OBJ_ACK, header_len + sizeof (GCSTelemetryStats), UAVTALK_OP150202_GCSTELEMETRYSTATS_OBJID);
+	GCSTelemetryStats data;
+	data.Status = status;
+	send (h, (uint8_t *) &data, sizeof (GCSTelemetryStats));
+}
+
+inline status::connection_state_t fts_respond (uint8_t state)
+{
+	if (state == FLIGHTTELEMETRYSTATS_STATUS_DISCONNECTED)
+	{
+		send_gcs_telemetry_stats (GCSTELEMETRYSTATS_STATUS_HANDSHAKEREQ);
+		return status::ESTABLISHING;
+	}
+
+	if (state == FLIGHTTELEMETRYSTATS_STATUS_HANDSHAKEACK)
+		send_gcs_telemetry_stats (GCSTELEMETRYSTATS_STATUS_CONNECTED);
+
+	request_object (release.flightstatus_objid);
+	return status::CONNECTED;
+}
+
+void handle_flighttelemetrystats ()
+{
+	FlightTelemetryStats *obj = (FlightTelemetryStats *) &buffer.data;
+	status::connection = fts_respond (obj->Status);
+	connection_timeout = update_time + UAVTALK_CONNECTION_TIMEOUT;
+}
+
+void handle_gpspositionsensor ()
+{
+	GPSPositionSensor *obj = (GPSPositionSensor *) &buffer.data;
+	gps::latitude   = obj->Latitude / 10000000.0;
+	gps::longitude  = obj->Longitude / 10000000.0;
+	gps::altitude   = obj->Altitude;
+	gps::heading    = round (obj->Heading);
+	stable::groundspeed = gps::speed = obj->Groundspeed;
+	gps::state      = (gps_state_t) obj->Status;
+	gps::satellites = obj->Satellites;
+#if !defined (TELEMETRY_MODULES_I2C_COMPASS)
+	if (stable::heading_source == stable::DISABLED
+		|| stable::heading_source == stable::GPS)
+	{
+		stable::heading = gps::heading;
+		stable::heading_source = stable::GPS;
+	}
+#endif
+#if !defined (TELEMETRY_MODULES_I2C_BARO)
+	// update stable altitude if we can't get the baro altitude
+	if (!baro_enabled) stable::update_alt_climb (gps::altitude);
+#endif
+	// calc home distance/direction based on gps
+	if (internal_home_calc) home::update ();
+}
+
+void handle_gpsvelocitysensor ()
+{
+	GPSVelocitySensor *obj = (GPSVelocitySensor *) &buffer.data;
+	gps::climb = -obj->Down;
+}
+
+void handle_manualcontrolcommand ()
+{
+	ManualControlCommand *obj = (ManualControlCommand *) &buffer.data;
+	input::throttle   = (int8_t) (obj->Throttle * 100);
+	input::roll       = (int8_t) (obj->Roll * 100);
+	input::pitch      = (int8_t) (obj->Pitch * 100);
+	input::yaw        = (int8_t) (obj->Yaw * 100);
+	memcpy (input::channels, obj->Channel, sizeof (obj->Channel));
+	input::connected = obj->Connected;
+#if !defined (TELEMETRY_MODULES_ADC_RSSI)
+	input::set_rssi (input::connected ? 100 : 0);
+#endif
+}
+
+void handle_positionstate ()
+{
+	if (internal_home_calc) return;
+
+	home::state = home::FIXED;
+
+	PositionState *obj = (PositionState *) &buffer.data;
+	home::distance = sqrt (square (obj->East) + square (obj->North));
+	int16_t bearing = atan2 (obj->East, obj->North) * 57.295775;
+	if (bearing < 0) bearing += 360;
+	if (bearing > 360) bearing -= 360;
+	home::direction = bearing;
+}
+
+#if !defined (TELEMETRY_MODULES_I2C_COMPASS)
+void handle_magsensor ()
+{
+	MagSensor *obj = (MagSensor *) &buffer.data;
+	if (mag_enabled) return;
+
+	stable::heading_source = stable::INTERNAL_MAG;
+	stable::calc_heading (obj->x, obj->y);
+}
+
+void handle_magstate ()
+{
+	MagState *obj = (MagState *) &buffer.data;
+	if (obj->Source == MAGSTATE_SOURCE_INVALID) return;
+
+	stable::heading_source = obj->Source == MAGSTATE_SOURCE_ONBOARD
+		? stable::INTERNAL_MAG
+		: stable::EXTERNAL_MAG;
+	stable::calc_heading (obj->x, obj->y);
+	mag_enabled = true;
+}
+#endif
+
+void handle_systemstats ()
+{
+	SystemStats *obj = (SystemStats *) &buffer.data;
+	status::flight_time = obj->FlightTime / 1000;
+}
+
+void update_connection ()
+{
+	send_gcs_telemetry_stats (GCSTELEMETRYSTATS_STATUS_CONNECTED);
+}
+
+}  // namespace op150202
+
+UT_NAMESPACE_CLOSE
