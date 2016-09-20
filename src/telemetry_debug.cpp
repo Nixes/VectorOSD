05bd4a26c111374dbe0f1591921774c39aaefa1d
Nixes <callum.l.bryant@gmail.com>
2016-09-20 18:40:12 +1000

    handle error listing devices

--- src/telemetry_debug.cpp
+++ src/telemetry_debug.cpp
@@ -11,22 +11,26 @@ void list_ports() {
   int i;
   struct sp_port **ports;
 
-  sp_list_ports(&ports);
-  for (i = 0; ports[i]; i++) {
-    printf("Found port: '%s'.\n", sp_get_port_name(ports[i]));
+  sp_return error = sp_list_ports(&ports);
+  if (error == SP_OK) {
+    for (i = 0; ports[i]; i++) {
+      printf("Found port: '%s'.\n", sp_get_port_name(ports[i]));
+    }
+    sp_free_port_list(ports);
+  } else {
+    printf("No serial devices detected\n");
   }
-
-  sp_free_port_list(ports);
 }
 
 int main() {
+  // this segfaults if there are no serial devices connected
   list_ports();
 
   printf("Opening port %s \n", desired_port);
   struct sp_port *port;
-  sp_get_port_by_name(desired_port,&port);
+  //sp_get_port_by_name(desired_port,&port);
 
-  sp_open(port,SP_MODE_READ_WRITE );
+  //sp_open(port,SP_MODE_READ_WRITE );
 
   return 0;
 }
