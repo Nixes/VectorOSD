82ba69ae90c4465ed271c571a2ae46e89a1d169f
Nixes <callum@see-bry.com>
2016-09-20 13:49:18 +1000

    added port list and opening

--- src/telemetry_debug.cpp
+++ src/telemetry_debug.cpp
@@ -5,7 +5,9 @@
 #include <libserialport.h> // cross platform serial port lib
 //#include "protocol.h"
 
-int main() {
+const char* desired_port = "COM5";
+
+void list_ports() {
   int i;
   struct sp_port **ports;
 
@@ -13,7 +15,18 @@ int main() {
   for (i = 0; ports[i]; i++) {
     printf("Found port: '%s'.\n", sp_get_port_name(ports[i]));
   }
-  
+
   sp_free_port_list(ports);
+}
+
+int main() {
+  list_ports();
+
+  printf("Opening port %s \n", desired_port);
+  struct sp_port *port;
+  sp_get_port_by_name(desired_port,&port);
+
+  sp_open(port,SP_MODE_READ_WRITE );
+
   return 0;
 }
