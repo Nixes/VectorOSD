346afb1c911b7284f597e983295a569f9b35ad6e
Nixes <callum@see-bry.com>
2016-09-21 11:47:34 +1000

    even more error handling

--- src/telemetry_debug.cpp
+++ src/telemetry_debug.cpp
@@ -2,10 +2,12 @@
 #include <string.h>
 #include <math.h>
 
+#include <unistd.h> // for sleep function
+
 #include <libserialport.h> // cross platform serial port lib
 //#include "protocol.h"
 
-const char* desired_port = "COM5";
+const char* desired_port = "COM8";
 
 void list_ports() {
   int i;
@@ -25,18 +27,30 @@ void list_ports() {
 
 int main() {
   list_ports();
+  sleep(1);
 
   printf("Opening port '%s' \n", desired_port);
   struct sp_port *port;
   sp_return error = sp_get_port_by_name(desired_port,&port);
   if (error == SP_OK) {
-    sp_set_baudrate(port,9600);
-    sp_open(port,SP_MODE_READ_WRITE );
+    sp_set_baudrate(port,57600);
+    error = sp_open(port,SP_MODE_READ);
+    if (error == SP_OK) {
+      bool something = true;
+      while(something) {
+        //sleep(2);
+        int bytes_waiting = sp_input_waiting(port);
+        if (bytes_waiting > 0) {
+          printf("Bytes waiting %i\n", bytes_waiting);
+        }
+      }
 
-
-    sp_close(port);
+      sp_close(port);
+    } else {
+      printf("Error opening serial device\n");
+    }
   } else {
-    printf("Error opening serial device\n");
+    printf("Error finding serial device\n");
   }
 
 
